e793283aed6f4467890b6043c137e11fc1ba0421
shaoern0606 <shaoern0606@gmail.com>
2025-11-18 15:10:59 +0800

    Move SmartjournalingApplication.java out of backend folder

--- smartjournaling/src/main/java/com/example/smartjounaling/SmartjournalingApplication.java
+++ smartjournaling/src/main/java/com/example/smartjounaling/SmartjournalingApplication.java
@@ -1,4 +1,4 @@
-package com.example.smartjounaling.backend;
+package com.example.smartjounaling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

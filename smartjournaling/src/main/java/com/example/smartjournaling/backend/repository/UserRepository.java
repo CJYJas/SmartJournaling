da593d1e97856ff4002ec908195e5a2b427f4de6
CJYJas <jasminecju06@gmail.com>
2025-11-21 08:37:09 +0800

    Trying

---
+++ smartjournaling/src/main/java/com/example/smartjournaling/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.smartjournaling.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.smartjournaling.backend.model.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByEmail(String email);
+}
